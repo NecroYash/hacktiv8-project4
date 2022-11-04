764463b001ea8d76334cda46aa170f0864246903
Muhxdan <danufirdaus51@gmail.com>
2022-11-04 23:09:35 +0700

    update profile & try fix bug login

--- app/src/main/java/com/example/travel/LoginSignupActivity.java
+++ app/src/main/java/com/example/travel/LoginSignupActivity.java
@@ -3,15 +3,12 @@ package com.example.travel;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -22,7 +19,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginSignupActivity extends AppCompatActivity implements View.OnClickListener {
@@ -35,8 +31,6 @@ public class LoginSignupActivity extends AppCompatActivity implements View.OnCli
     @Override
     protected void onStart() {
         super.onStart();
-
-        createRequest();
     }
 
     @Override
@@ -47,7 +41,7 @@ public class LoginSignupActivity extends AppCompatActivity implements View.OnCli
         linearButton = (LinearLayout) findViewById(R.id.linear_button_login);
         linearButton.setOnClickListener(this);
         loading = (ProgressBar) findViewById(R.id.loadingLogin);
-
+        createRequest();
     }
 
     @Override
@@ -64,27 +58,24 @@ public class LoginSignupActivity extends AppCompatActivity implements View.OnCli
             Task<GoogleSignInAccount> signInAccountTask= GoogleSignIn.getSignedInAccountFromIntent(data);
             if(signInAccountTask.isSuccessful()) {
                 try {
-                    GoogleSignInAccount googleSignInAccount=signInAccountTask.getResult(ApiException.class);
-                    if(googleSignInAccount!=null) {
-                        AuthCredential authCredential= GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(),null);
-                        auth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        if(task.isSuccessful()) {
-                                            loading.setVisibility(View.GONE);
-                                            startActivity(new Intent(LoginSignupActivity.this ,MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                                            finish();
-                                        }
-                                        else {
-                                            loading.setVisibility(View.GONE);
-                                            Toast.makeText(LoginSignupActivity.this, "Authentication Failed : "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                        }
+                    GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
+                    AuthCredential authCredential= GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(),null);
+                    auth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()) {
+                                        loading.setVisibility(View.GONE);
+                                        startActivity(new Intent(LoginSignupActivity.this ,MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                                        finish();
                                     }
-                                });
-                    }
+                                    else {
+                                        loading.setVisibility(View.GONE);
+                                        Toast.makeText(LoginSignupActivity.this, "Authentication Failed : "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
-                catch (ApiException e)
-                {
+                catch (ApiException e) {
                     e.printStackTrace();
                 }
             }
@@ -92,9 +83,9 @@ public class LoginSignupActivity extends AppCompatActivity implements View.OnCli
     }
 
     private void createRequest(){
-        GoogleSignInOptions googleSignInOptions=new GoogleSignInOptions.Builder(
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(
                 GoogleSignInOptions.DEFAULT_SIGN_IN
-        ).requestIdToken("277537866096-rdn54nnj7iap684r7q6l4d479ccid31a.apps.googleusercontent.com")
+        ).requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -102,7 +93,7 @@ public class LoginSignupActivity extends AppCompatActivity implements View.OnCli
     }
 
     private void signin(){
-        Intent intent=googleSignInClient.getSignInIntent();
+        Intent intent = googleSignInClient.getSignInIntent();
         startActivityForResult(intent, 100);
     }
 }
--- app/src/main/java/com/example/travel/UserProfileActivity.java
+++ app/src/main/java/com/example/travel/UserProfileActivity.java
@@ -1,14 +1,29 @@
 package com.example.travel;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -16,15 +31,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.checkerframework.checker.units.qual.C;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserProfileActivity extends AppCompatActivity {
+public class UserProfileActivity extends AppCompatActivity implements View.OnClickListener {
     FirebaseAuth auth;
     GoogleSignInClient googleSignInClient;
 
-    Button buttonLogout;
-
+    ConstraintLayout buttonBooking, buttonSpending, buttonInformation, buttonLogout;
+    ImageView imageProfile, buttonBack;
+    TextView nameProfile, emailProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,30 +51,101 @@ public class UserProfileActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
-        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        imageProfile = (ImageView) findViewById(R.id.imageProfile);
+        nameProfile = (TextView) findViewById(R.id.nameProfile);
+        emailProfile = (TextView) findViewById(R.id.emailProfile);
 
-        googleSignInClient= GoogleSignIn.getClient(UserProfileActivity.this
-                , GoogleSignInOptions.DEFAULT_SIGN_IN);
-        buttonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+        buttonBack = (ImageView) findViewById(R.id.buttonBackProfile);
+        buttonBooking = (ConstraintLayout) findViewById(R.id.buttonBooking);
+        buttonSpending = (ConstraintLayout) findViewById(R.id.buttonSpending);
+        buttonInformation = (ConstraintLayout) findViewById(R.id.buttonInformation);
+        buttonLogout = (ConstraintLayout) findViewById(R.id.buttonLogout);
+
+        buttonBack.setOnClickListener(this);
+        buttonBooking.setOnClickListener(this);
+        buttonSpending.setOnClickListener(this);
+        buttonInformation.setOnClickListener(this);
+        buttonLogout.setOnClickListener(this);
+
+        googleSignInClient = GoogleSignIn.getClient(UserProfileActivity.this, GoogleSignInOptions.DEFAULT_SIGN_IN);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case (R.id.buttonBackProfile) :
+                onBackPressed();
+                break;
+            case (R.id.buttonBooking) :
+                Toast.makeText(this, "Booking button", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.buttonSpending) :
+                Toast.makeText(this, "Total Spending button", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.buttonInformation) :
+                Toast.makeText(this, "Information button", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.buttonLogout) :
+                dialog();
+                break;
+        }
+    }
+
+    private void showData(){
+        nameProfile.setText(auth.getCurrentUser().getDisplayName());
+        emailProfile.setText(auth.getCurrentUser().getEmail());
+
+        Glide.with(this.getApplicationContext())
+                .load(auth.getCurrentUser().getPhotoUrl())
+                .listener(new RequestListener<Drawable>() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()) {
-                            auth.signOut();
-
-                            Toast.makeText(getApplicationContext(), "Logout successful", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(UserProfileActivity.this, LoginSignupActivity.class);
-                            startActivity(intent);
-
-                            finish();
-                        }
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
                     }
-                });
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        return false;
+                    }
+                })
+                .error(R.drawable.ic_launcher_background)
+                .into(imageProfile);
+    }
+
+    private void logout(){
+        googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()) {
+                    auth.signOut();
+                    Intent intent = new Intent(UserProfileActivity.this, LoginSignupActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
     }
 
+    private void dialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Log out").setMessage("You will be returned to the login screen.").setCancelable(false).setPositiveButton("Log out", (DialogInterface.OnClickListener) (dialog, which) -> {
+            logout();
+        }).setNegativeButton("Cancel", (DialogInterface.OnClickListener) (dialog, which) -> {
+            dialog.cancel();
+        });
 
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showData();
+    }
 }
